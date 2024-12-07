a52c1f82439d7f1640000d8eaa53cd5094aea644
SergeyDenisenko <denisenko.sa@ffx.media>
2024-12-07 23:15:02 +0000

    Homework 1

---
+++ hw1/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <ostream>
+
+int main() {
+    std::cout << "=========================" << std::endl;
+    std::cout << "Hello," << std::endl;
+    std::cout << "World" << std::endl;
+    std::cout << "=========================" << std::endl;
+
+    std::cout << std::endl;
+    std::cout << "=========================" << std::endl;
+    std::cout << "||        Hello,       ||" << std::endl;
+    std::cout << "||        World        ||" << std::endl;
+    std::cout << "=========================" << std::endl;
+
+    std::cout << std::endl;
+    std::cout << "=========================" << std::endl;
+    std::cout << "||                     ||" << std::endl;
+    std::cout << "||                     ||" << std::endl;
+    std::cout << "||        Hello,       ||" << std::endl;
+    std::cout << "||        World        ||" << std::endl;
+    std::cout << "||                     ||" << std::endl;
+    std::cout << "||                     ||" << std::endl;
+    std::cout << "=========================" << std::endl;
+
+    return 0;
+}
